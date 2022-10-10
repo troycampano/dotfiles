fdd6fad4e31f297720c0e3cf7a5860a096673561
Troy Campano <troygeek@gmail.com>
2022-10-10 19:00:32 -0400

    qemu update, remove cdrom

--- .scripts/qemu-ubuntu.sh
+++ .scripts/qemu-ubuntu.sh
@@ -14,3 +14,17 @@ qemu-system-x86_64 \
   -device ich9-intel-hda -device hda-output,audiodev=coreaudio \
   -cdrom ubuntu-22.04.1-desktop-amd64.iso \
   -drive file=UB01.qcow2,if=virtio 
+
+  qemu-system-x86_64 \
+  -accel hvf \
+  -cpu host \
+  -smp 2 \
+  -m 4G \
+  -usb \
+  -device usb-tablet \
+  -vga virtio \
+  -display default,show-cursor=on \
+  -device virtio-net,netdev=vmnic -netdev user,id=vmnic \
+  -audiodev coreaudio,id=coreaudio \
+  -device ich9-intel-hda -device hda-output,audiodev=coreaudio \
+  -drive file=UB01.qcow2,if=virtio 
